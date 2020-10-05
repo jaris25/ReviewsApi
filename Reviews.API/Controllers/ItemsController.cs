25d39e48e70a74672be97663ef6660d4a4e9ca8f
Jaroslav Miliunec <jaroslav.miliunec@macaw.nl>
2020-10-05 10:58:31 +0300

    init commit

---
+++ Reviews.API/Controllers/ItemsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.API.Controllers
+{
+    public class ItemsController
+    {
+    }
+}
